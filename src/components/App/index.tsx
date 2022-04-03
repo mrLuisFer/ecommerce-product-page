d83b9b7bee79ef84aea1034ad7f698ec54175612
mrLuisFer <mrluisfeer@gmail.com>
2022-04-03 01:23:18 -0600

    first commit

---
+++ src/components/App/index.tsx
@@ -0,0 +1,5 @@
+import Header from "../Header";
+
+export default function App() {
+  return <Header />;
+}
---
+++ src/main.tsx
@@ -0,0 +1,5 @@
+import { StrictMode } from "react";
+import { render } from "react-dom";
+import "./index.css";
+
+render(<StrictMode></StrictMode>, document.getElementById("root"));
