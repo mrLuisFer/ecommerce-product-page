0e4c5b7e116919d9321708dde6b74db6d00c2fe8
mrLuisFer <mrluisfeer@gmail.com>
2022-04-10 19:48:35 -0500

    fix details for the carousel

--- src/components/MobileProduct/index.tsx
+++ src/components/MobileProduct/index.tsx
@@ -1,20 +1,43 @@
-import {useState} from "react"
+import { useState } from "react";
 import nextArrowIcon from "@/images/icons/icon-next.svg";
 import prevArrowIcon from "@/images/icons/icon-previous.svg";
-import productImages from "./productImages"
+import productImages from "./productImages";
+import styles from "./carousel.module.css";
 
 export default function MobileProduct() {
+  const [imgIndex, setImgIndex] = useState<number>(0);
 
-  const [imgIndex, setImgIndex] = useState<number>(0)
+  const handleIncrementIndex = () => {
+    console.log(imgIndex);
+    if (imgIndex < 3) {
+      setImgIndex((prevImg) => prevImg + 1);
+    } else setImgIndex(0);
+  };
+
+  const handleDecrementIndex = () => {
+    if (imgIndex > 0) {
+      setImgIndex((prevImg) => prevImg - 1);
+    } else setImgIndex(3);
+  };
 
   return (
-    <section>
-      <button>
-        <img src={nextArrowIcon} alt="next" />
-      </button>
-      <img src={productImages[imgIndex]} alt="Product thumbnail" />
-      <button>
+    <section className={styles.mobileProduct}>
+      <button
+        className={`${styles.carouselBtn} ${styles.leftBtn}`}
+        onClick={handleDecrementIndex}
+      >
         <img src={prevArrowIcon} alt="previous" />
+      </button>
+      <img
+        className={styles.productImg}
+        src={productImages[imgIndex]}
+        alt="Product thumbnail"
+      />
+      <button
+        className={`${styles.carouselBtn} ${styles.rightBtn}`}
+        onClick={handleIncrementIndex}
+      >
+        <img src={nextArrowIcon} alt="next" />
       </button>
     </section>
   );
--- src/components/MobileProduct/productImages.ts
+++ src/components/MobileProduct/productImages.ts
@@ -1,13 +1,8 @@
-import img1 from "@/images/image-product-1-thumbnail.jpg"
-import img2 from "@/images/image-product-2-thumbnail.jpg"
-import img3 from "@/images/image-product-3-thumbnail.jpg"
-import img4 from "@/images/image-product-4-thumbnail.jpg"
+import img1 from "@/images/image-product-1.jpg";
+import img2 from "@/images/image-product-2.jpg";
+import img3 from "@/images/image-product-3.jpg";
+import img4 from "@/images/image-product-4.jpg";
 
-const productImages = [
-	img1,
-	img2,
-	img3,
-	img4
-]
+const productImages = [img1, img2, img3, img4];
 
-export default productImages
+export default productImages;
