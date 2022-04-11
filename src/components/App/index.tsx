079700fee93ac392aa30a90bff4de4a75b4f019e
mrLuisFer <mrluisfeer@gmail.com>
2022-04-10 19:58:14 -0500

    create product description component

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,11 +1,13 @@
 import Header from "../Header";
 import MobileProduct from "../MobileProduct";
+import ProductDescription from "../ProductDescription";
 
 export default function App() {
   return (
     <>
       <Header />
       <MobileProduct />
+      <ProductDescription />
     </>
   );
 }
---
+++ src/components/ProductDescription/index.tsx
@@ -0,0 +1,29 @@
+export default function ProductDescription() {
+  return (
+    <section>
+      <div>
+        <p>Sneaker Company</p>
+        <h1>Fall Limited Edition Sneakers</h1>
+        <p>
+          These low-profile sneakers are your perfect casual wear companion.
+          Featuring a durable rubber outer sole, they'll whithstand everything
+          the weather can offer.
+        </p>
+      </div>
+      <div>
+        <h2>$125.00</h2>
+        <div>
+          <p>50%</p>
+        </div>
+        <span>$250.00</span>
+      </div>
+
+      <div>
+        <button>-</button>
+        <p>0</p>
+        <button>+</button>
+      </div>
+      <button>Add to cart</button>
+    </section>
+  );
+}
