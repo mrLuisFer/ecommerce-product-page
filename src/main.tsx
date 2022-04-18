b97d1c0c130c69f2863437a57cc34e3d0469cb0b
mrLuisFer <mrluisfeer@gmail.com>
2022-04-18 08:36:43 -0500

    add a validation if node_env is in development for strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,15 @@
 import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
 import "./css/index.css";
 import App from "./components/App";
 
 const app = document.getElementById("root")!;
 const root = createRoot(app);
-root.render(<App />);
+
+process.env.NODE_ENV === "development"
+  ? root.render(
+      <StrictMode>
+        <App />
+      </StrictMode>
+    )
+  : root.render(<App />);
