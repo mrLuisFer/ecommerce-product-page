6758b52a7ee8a46483333b45dd49753297b8b3fc
mrLuisFer <mrluisfeer@gmail.com>
2022-05-14 13:15:48 -0500

    fix styles and add reac-dom types

---
+++ src/components/Header/CartModal.tsx
@@ -0,0 +1,3 @@
+export default function CartModal() {
+  return <div>CartModal</div>;
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,36 +1,52 @@
+import { createPortal } from "react-dom";
+import { useState } from "react";
 import menuIcon from "@/images/icons/icon-menu.svg";
 import logoIcon from "@/images/icons/logo.svg";
 import cartIcon from "@/images/icons/icon-cart.svg";
 import avatarImg from "@/images/image-avatar.png";
 import classes from "./header.module.css";
 import { Dispatch, SetStateAction } from "react";
+import CartModal from "./CartModal";
 
 interface HeaderProps {
   setMobileMenu: Dispatch<SetStateAction<boolean>>;
 }
 
 export default function Header({ setMobileMenu }: HeaderProps) {
+  const [showCartModal, setShowCartModal] = useState(false);
+  const cartModalElement = document.getElementById("cartModal")!;
+
   return (
-    <header className={classes.header}>
-      <section className={classes.alignCenter}>
-        <div
-          className={classes.cursorPointer}
-          onClick={() => setMobileMenu(true)}
-        >
-          <img src={menuIcon} alt="Menu Icon" className={classes.menuIcon} />
-        </div>
-        <div className={classes.cursorPointer}>
-          <img src={logoIcon} alt="Sneakers Logo" />
-        </div>
-      </section>
-      <section className={classes.alignCenter}>
-        <div className={classes.cursorPointer}>
-          <img src={cartIcon} alt="Add to Cart" className={classes.cartIcon} />
-        </div>
-        <div className={classes.cursorPointer}>
-          <img src={avatarImg} alt="Avatar" className={classes.avatar} />
-        </div>
-      </section>
-    </header>
+    <>
+      <header className={classes.header}>
+        <section className={classes.alignCenter}>
+          <div
+            className={classes.cursorPointer}
+            onClick={() => setMobileMenu(true)}
+          >
+            <img src={menuIcon} alt="Menu Icon" className={classes.menuIcon} />
+          </div>
+          <div className={classes.cursorPointer}>
+            <img src={logoIcon} alt="Sneakers Logo" />
+          </div>
+        </section>
+        <section className={classes.alignCenter}>
+          <button
+            className={`${classes.btn} ${classes.cursorPointer}`}
+            onClick={() => setShowCartModal((prevState) => !prevState)}
+          >
+            <img
+              src={cartIcon}
+              alt="Add to Cart"
+              className={classes.cartIcon}
+            />
+          </button>
+          <div className={classes.cursorPointer}>
+            <img src={avatarImg} alt="Avatar" className={classes.avatar} />
+          </div>
+        </section>
+      </header>
+      {showCartModal && createPortal(<CartModal />, cartModalElement)}
+    </>
   );
 }
