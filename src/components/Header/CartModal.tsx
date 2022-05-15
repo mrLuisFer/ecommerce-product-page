3d406e50e10ce97efa0a54bb8892851ca118f852
Luis Alvarez <mrluisfeer@gmail.com>
2022-05-15 11:07:54 -0500

    fix styles of cart item modal

--- src/components/Header/CartModal.tsx
+++ src/components/Header/CartModal.tsx
@@ -1,46 +1,51 @@
-import { useCartContext } from "../context/CartItemsCtx";
-import classes from "./cartModal.module.css";
-import iconDelete from "@/images/icons/icon-delete.svg";
-import productThumbnail from "@/images/image-product-1-thumbnail.jpg";
-import type { ICartItemCtx } from "../context/CartItemsCtx";
+import {useCartContext} from '../context/CartItemsCtx';
+import classes from './cartModal.module.css';
+import iconDelete from '@/images/icons/icon-delete.svg';
+import productThumbnail from '@/images/image-product-1-thumbnail.jpg';
+import type {ICartItemCtx} from '../context/CartItemsCtx';
 
-const Dialog = ({ itemsCounter }: ICartItemCtx) => {
+const ProductItem = ({itemsCounter}: ICartItemCtx) => {
   return (
-    <div>
-      <section>
-        <img src={productThumbnail} alt="Product Image" />
-        <div>
-          <h3>Autumn Limited Edition..</h3>
-          <p>
+    <section className={classes.productItemContainer}>
+      <div className={classes.productItem}>
+        <img
+          src={productThumbnail}
+          alt="Product Image"
+          className={classes.productItemImg}
+        />
+        <div className={classes.productInfo}>
+          <h3 className={classes.productTitle}>Autumn Limited Edition..</h3>
+          <p className={classes.productText}>
             $125.00 x {itemsCounter} <span>${itemsCounter * 125}.00</span>
           </p>
-          <button type="button">
-            <img src={iconDelete} alt="Clear Cart" />
-          </button>
         </div>
-      </section>
-      <button type="button">Checkout</button>
-    </div>
+        <button type="button" className={classes.productDeleteBtn}>
+          <img src={iconDelete} alt="Clear Cart" />
+        </button>
+      </div>
+      <button type="button" className={classes.checkoutBtn}>
+        Checkout
+      </button>
+    </section>
   );
 };
 
 export default function CartModal() {
-  const { addItem, itemsCounter, setAddItem, setItemsCounter } =
-    useCartContext();
+  const {addItem, itemsCounter, setAddItem, setItemsCounter} = useCartContext();
 
   return (
     <div className={classes.modalContainer}>
       <h2 className={classes.title}>Cart</h2>
       <div className={classes.cartItemsContainer}>
         {addItem ? (
-          <Dialog
+          <ProductItem
             itemsCounter={itemsCounter}
             addItem={addItem}
             setAddItem={setAddItem}
             setItemsCounter={setItemsCounter}
           />
         ) : (
-          <p>Your cart is empty.</p>
+          <p className={classes.cartEmptyText}>Your cart is empty.</p>
         )}
       </div>
     </div>
